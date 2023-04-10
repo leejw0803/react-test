79b2262d28c737629883541a4fc9e94abfe82fda
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2023-04-10 14:36:32 +0900

    chore: add libraries and set some configuration for testing

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
+  },
+};
